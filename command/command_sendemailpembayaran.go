68e1b7075eedbef936e79b42b93bde069777ee00
oprahadian <oprahadian@gmail.com>
2019-08-23 11:10:27 +0700

    sendemailpembayaran to mailgun

--- command/command_sendemailpembayaran.go
+++ command/command_sendemailpembayaran.go
@@ -15,32 +15,54 @@ func CommandSendEmailPembayaran(){
 		RequestDate string
 	}
 
-	var emailTemplateStr = `
-	<!DOCTYPE html>
-	<html>
+	// var emailTemplateStr = `
+	// <!DOCTYPE html>
+	// <html>
 	
-	<head> </head>
+	// <head> </head>
 	
-	<body>
-		<p style="margin: 0in 0in 8pt; line-height: 107%; font-size: 11pt; font-family: Calibri, sans-serif;"><strong><em><span style="font-size: 14.0pt; color: black;">Mohon segera selesaikan pembayaran anda sebelum Kamis, 02 Agustus 2019 23:59.</span></em></strong></p>
-		<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><strong><em><span style="color: black;">&nbsp;</span></em></strong></p>
-		<p style="margin: 0in 0in 8pt; line-height: 11.75pt; font-size: 12pt; font-family: 'Times New Roman', serif;"><span style="font-size: 11.0pt; font-family: Calibri, sans-serif; color: black;">Rincian Pesanan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Tiket Early Bird Single Package</span></p>
-		<p style="margin: 0in 0in 8pt; line-height: 11.75pt; font-size: 12pt; font-family: 'Times New Roman', serif;"><span style="font-size: 11.0pt; font-family: Calibri, sans-serif; color: black;">Jumlah&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;: 1</span></p>
-		<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">&nbsp;</span></p>
-		<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">Hi, </span><span style="font-size: 14.0pt; color: black;"> {{ .Name }} </span></p>
-		<p style="line-height: 106%; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="font-size: 12.0pt; line-height: 106%; color: black;">Silakan lakukan pembayaran ke rekening berikut:</span></p>
-		<p style="line-height: 106%; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="font-size: 12.0pt; line-height: 106%; color: black;">Nomor Rekening&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : <strong>07-0000-680-7106 (Bank Mandiri)</strong></span></p>
-		<p style="line-height: 106%; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="font-size: 12.0pt; line-height: 106%; color: black;">Nama Pemilik &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : <strong>Yayasan Alumni Wikusama SMK Telkom Malang.</strong></span></p>
-		<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">Total yang harus dibayar&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span style="font-size: 14.0pt; color: black;">Rp {{ .Price }} </span></p>
-		<p style="margin: 0in 0in 8pt 0.5in; line-height: 11.75pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="font-size: 10.0pt; color: black;">(mohon ditransfer beserta kode uniknya, untuk memudahkan kami mengidentifikasi pembayaran)</span></p>
-		<p style="margin: 0in 0in 8pt 0.5in; line-height: 11.75pt; font-size: 11pt; font-family: Calibri, sans-serif;">&nbsp;</p>
-		<p style="text-align: justify; line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">Jika ada pertanyaan, silakan email ke publikasi@wikusama.org atau dapat menghubungi (via whatsapp chat) ke +62 896-1349-0734.</span></p>
-	</body>
-	</html>
-	`
+	// <body>
+	// 	<p style="margin: 0in 0in 8pt; line-height: 107%; font-size: 11pt; font-family: Calibri, sans-serif;"><strong><em><span style="font-size: 14.0pt; color: black;">Mohon segera selesaikan pembayaran anda sebelum Kamis, 29 Agustus 2019 23:59.</span></em></strong></p>
+	// 	<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><strong><em><span style="color: black;">&nbsp;</span></em></strong></p>
+	// 	<p style="margin: 0in 0in 8pt; line-height: 11.75pt; font-size: 12pt; font-family: 'Times New Roman', serif;"><span style="font-size: 11.0pt; font-family: Calibri, sans-serif; color: black;">Rincian Pesanan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Tiket Regular Family Package</span></p>
+	// 	<p style="margin: 0in 0in 8pt; line-height: 11.75pt; font-size: 12pt; font-family: 'Times New Roman', serif;"><span style="font-size: 11.0pt; font-family: Calibri, sans-serif; color: black;">Jumlah&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;: 1</span></p>
+	// 	<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">&nbsp;</span></p>
+	// 	<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">Hi, </span><span style="font-size: 14.0pt; color: black;"> {{ .Name }} </span></p>
+	// 	<p style="line-height: 106%; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="font-size: 12.0pt; line-height: 106%; color: black;">Silakan lakukan pembayaran ke rekening berikut:</span></p>
+	// 	<p style="line-height: 106%; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="font-size: 12.0pt; line-height: 106%; color: black;">Nomor Rekening&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : <strong>07-0000-680-7106 (Bank Mandiri)</strong></span></p>
+	// 	<p style="line-height: 106%; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="font-size: 12.0pt; line-height: 106%; color: black;">Nama Pemilik &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : <strong>Yayasan Alumni Wikusama SMK Telkom Malang.</strong></span></p>
+	// 	<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">Total yang harus dibayar&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span style="font-size: 14.0pt; color: black;">Rp {{ .Price }} </span></p>
+	// 	<p style="margin: 0in 0in 8pt 0.5in; line-height: 11.75pt; font-size: 11pt; font-family: Calibri, sans-serif;">&nbsp;</p>
+	// 	<p style="text-align: justify; line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">Jika ada pertanyaan, silakan email ke publikasi@wikusama.org atau dapat menghubungi (via whatsapp chat) ke +62 896-1349-0734.</span></p>
+	// </body>
+	// </html>
+	// `
+
+	// var emailTemplateStr = `
+	// <!DOCTYPE html>
+	// <html>
+	
+	// <head> </head>
+	
+	// <body>
+	// 	<p style="margin: 0in 0in 8pt; line-height: 107%; font-size: 11pt; font-family: Calibri, sans-serif;"><strong><em><span style="font-size: 14.0pt; color: black;">Mohon segera selesaikan pembayaran anda sebelum Kamis, 29 Agustus 2019 23:59.</span></em></strong></p>
+	// 	<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><strong><em><span style="color: black;">&nbsp;</span></em></strong></p>
+	// 	<p style="margin: 0in 0in 8pt; line-height: 11.75pt; font-size: 12pt; font-family: 'Times New Roman', serif;"><span style="font-size: 11.0pt; font-family: Calibri, sans-serif; color: black;">Rincian Pesanan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Tiket Regular Single Package</span></p>
+	// 	<p style="margin: 0in 0in 8pt; line-height: 11.75pt; font-size: 12pt; font-family: 'Times New Roman', serif;"><span style="font-size: 11.0pt; font-family: Calibri, sans-serif; color: black;">Jumlah&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;: 1</span></p>
+	// 	<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">&nbsp;</span></p>
+	// 	<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">Hi, </span><span style="font-size: 14.0pt; color: black;"> {{ .Name }} </span></p>
+	// 	<p style="line-height: 106%; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="font-size: 12.0pt; line-height: 106%; color: black;">Silakan lakukan pembayaran ke rekening berikut:</span></p>
+	// 	<p style="line-height: 106%; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="font-size: 12.0pt; line-height: 106%; color: black;">Nomor Rekening&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : <strong>07-0000-680-7106 (Bank Mandiri)</strong></span></p>
+	// 	<p style="line-height: 106%; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="font-size: 12.0pt; line-height: 106%; color: black;">Nama Pemilik &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : <strong>Yayasan Alumni Wikusama SMK Telkom Malang.</strong></span></p>
+	// 	<p style="line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">Total yang harus dibayar&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span style="font-size: 14.0pt; color: black;">Rp {{ .Price }} </span></p>
+	// 	<p style="margin: 0in 0in 8pt 0.5in; line-height: 11.75pt; font-size: 11pt; font-family: Calibri, sans-serif;">&nbsp;</p>
+	// 	<p style="text-align: justify; line-height: 11.75pt; margin: 0in 0in 8pt; font-size: 11pt; font-family: Calibri, sans-serif;"><span style="color: black;">Jika ada pertanyaan, silakan email ke publikasi@wikusama.org atau dapat menghubungi (via whatsapp chat) ke +62 896-1349-0734.</span></p>
+	// </body>
+	// </html>
+	// `
+
 	var to = []EmailData{
-		EmailData{"", "", "", "",},
-	}
+		EmailData{"", "", "", "",},}
 	
 	tpl, _ := template.New("email").Parse(emailTemplateStr)
 
@@ -53,11 +75,12 @@ func CommandSendEmailPembayaran(){
 
 		fmt.Println(t)
 
-		common.CommonSendEmail(common.CommonSingleSendEmail{t.Email, 
+		common.CommonSendEmailMailgun(common.CommonSingleSendEmail{t.Email, 
 			[]string{},
-			[]string{},
+			[]string{"", "", ""},
 			"Konfirmasi Pesanan", 
 			tplBuffer.String(), 
 			""})
+
 	}
 }
